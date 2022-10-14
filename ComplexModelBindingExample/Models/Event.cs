2f6409ba2ffd495f9547c1a73d1c30eb4bf0c86e
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-13 19:44:42 -0700

    Implemented Event class and added properties.

---
+++ ComplexModelBindingExample/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComplexModelBindingExample.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string EventTitle { get; set; }
+
+        public string EventBy { get; set; }
+
+        public EventType Category { get; set; }
+
+    }
+}
