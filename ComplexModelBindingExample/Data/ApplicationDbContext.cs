3fba911c8595e83f69b970cd582a23c7d171422c
marysaray <82470838+marysaray@users.noreply.github.com>
2022-10-13 19:22:04 -0700

    Added DbSet to the database.

--- ComplexModelBindingExample/Data/ApplicationDbContext.cs
+++ ComplexModelBindingExample/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ComplexModelBindingExample.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ComplexModelBindingExample.Data
@@ -9,5 +10,7 @@ namespace ComplexModelBindingExample.Data
             : base(options)
         {
         }
+
+        public DbSet<EventType> Categories { get; set; }
     }
 }
